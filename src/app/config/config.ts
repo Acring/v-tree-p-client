06e0c85e729ec895f8dfcf58076063edb9b59803
Acring <392398434@qq.com>
2018-04-14 18:05:41 +0800

    修改配置端口

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,17 +1,17 @@
 export const config = {
   environment:{
-    debug: this
+    debug: true
   },
   server:{
     protocol: 'http',
     host: '123.56.29.170.',
     testHost: '127.0.0.1',
-    port: 5000,
+    port: 6607,
     testPort: 5000,
   },
   getApiPrefix: () => {
-    return `${config.server.protocol}://${config.environment.debug ? 
-      config.server.testHost+':'+config.server.testPort : config.server.host+':'+config.server.port}`;
+    return `${this.server.protocol}://${this.environment.debug ? 
+      this.server.testHost+':'+this.server.testPort : this.server.host+':'+this.server.port}`;
   },
 
 };
